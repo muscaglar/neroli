cfeab309cee2e35e83db5cbe1d9d984ad6416248
Mustafa Caglar <mc934@cam.ac.uk>
2018-05-22 13:14:38 -0700

    no message

--- neroli_remove_base.m
+++ neroli_remove_base.m
@@ -1,15 +1,13 @@
 function [current_fitted,time] = neroli_remove_base(current,fs)
 
-time = 0:1/fs:length(current)/fs;
-time(length(time)) = [];
-time = time';
+time = 0:1/fs:(length(current)-1)/fs;
 current_fitted_1 = detrend(current);
 %current_fitted_1 = current;
 
 opol = round(length(current)/50000);
-[p,s,mu] = polyfit(time,current_fitted_1,opol);
+[p,s,mu] = polyfit(time',current_fitted_1,opol);
 f_y = polyval(p,time,[],mu);
 
-current_fitted = current_fitted_1 - f_y;
+current_fitted = current_fitted_1 - f_y';
 
 end
--- untitled4.m
+++ untitled4.m
@@ -1,50 +1,60 @@
 
+trans = 1;
 
-[TF,P] = islocalmin(fil_current);
-    
-    for x = 1:length(TF)
-        if(P(x)<2)
-            TF(x) = 0;
+all_translocations = containers.Map('KeyType','double','ValueType','any');
+ecd = containers.Map('KeyType','double','ValueType','any');
+
+fs=100000;
+fc = 500;
+order = 2;
+
+time = 0:1/fs:(length(current)-1)/fs;
+[current, ~] = neroli_remove_base(current,fs);
+fil_current  = neroli_filter(fc,fs,current,'low',order);
+
+[TF,P] = islocalmin(fil_current,'MinProminence',0.15);
+
+padValue= 1;
+
+for i = 1:length(TF)-1
+    if(TF(i)== 1 && TF(i-1)==0)
+        if(i<padValue)
+            TF(1:i)=1;
+        else
+            TF(i-padValue:i)=1;
         end
     end
-    
-    padValue= 0;
-    
-    for i = 1:length(TF)-1
-        if(TF(i)== 1 && TF(i-1)==0)
-            if(i<padValue)
-                TF(1:i)=1;
-            else
-                TF(i-padValue:i)=1;
-            end
+end
+
+for i = length(TF)-1:-1:1
+    if(TF(i)== 1 && TF(i+1)==0)
+        if(i+padValue>length(TF))
+            TF(i:length(TF))=1;
+        else
+            TF(i:i+padValue)=1;
         end
     end
-    
-    for i = length(TF)-1:-1:1
-        if(TF(i)== 1 && TF(i+1)==0)
-            if(i+padValue>length(TF))
-                TF(i:length(TF))=1;
-            else
-                TF(i:i+padValue)=1;
-            end
-        end
-    end
-    
-    plot(time,fil_current,time(TF),fil_current(TF),'r*');
-    trans = 1;
-    i=1;
-    all_translocations = containers.Map('KeyType','double','ValueType','any');
-    while ( i<length(TF))
-        switch TF(i)
-            case 1
-                start = i;
-                while (TF(i)==1)
-                    i = i+1;
-                end
-                finish =i;
-                all_translocations(trans) = [fil_current(start:finish)',time(start:finish)'];
-                trans = trans+1;
-            case 0
+end
+
+plot(time,current,time(TF),current(TF),'r*');
+
+i=1;
+while ( i<length(TF))
+    switch TF(i)
+        case 1
+            start = i;
+            while (TF(i)==1)
                 i = i+1;
-        end
+            end
+            finish =i;
+            all_translocations(trans) = [current(start:finish)',time(start:finish)'];
+            trans = trans+1;
+        case 0
+            i = i+1;
     end
+end
+
+
+[good_translocations] = neroli_see_translocations(all_translocations);
+
+[ecds] = neroli_ECD(good_translocations);
